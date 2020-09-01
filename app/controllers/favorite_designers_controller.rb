647108871f42b0119a01e45215eabc4a90cf4eef
aromsa <annacbateman@gmail.com>
2020-09-01 17:39:33 -0400

    like button working ish

--- app/controllers/favorite_designers_controller.rb
+++ app/controllers/favorite_designers_controller.rb
@@ -1,2 +1,15 @@
 class FavoriteDesignersController < ApplicationController
+
+  def index
+    @favorite_designers = FavoriteDesigner.all
+    render json: @favorite_designers
+  end
+
+  def destroy
+    @favorite_designers = FavoriteDesigner.find(params[:id])
+    @favorite_designers.destroy
+   end
+
+
+
 end
